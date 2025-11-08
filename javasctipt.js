7c4e40d16da8f833e2f6ac175d38199f93af0711
Daniël Brandsema <danielbrandsema@gmail.com>
2025-11-08 16:23:12 +0100

    add getHumanChoice function using prompt
    creating a prompt for inputs ' rock', 'paper' or ' scissors'.

--- javasctipt.js
+++ javasctipt.js
@@ -3,17 +3,30 @@ function getRandomInt(min, max) {
   const maxFloored = Math.floor(4);
   return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
 }
+console.log(getRandomInt());
 
 const a = "rock";
 const b = "paper";
 const c = "scissors";
 
 function getComputerChoice(a,b,c) {
-    if (getRandomInt() == 1) {
+    if (getRandomInt() === 1) {
         return a;
-    } else if (getRandomInt() == 2) {
+    } else if (getRandomInt() === 2) {
         return b;
     } else return c;
  }
 
- console.log(getComputerChoice(a,b,c));  
+ console.log(getComputerChoice(a,b,c));
+
+ let answer = prompt("Enter 'rock', 'paper' or 'scissors'?")
+
+ function getHumanChoice(a,b,c) {
+    if (answer === 'rock') {
+        return a;
+    } else if (answer === 'paper') {
+        return b;
+    } else return c;
+ }
+
+ console.log(getHumanChoice(a,b,c));
