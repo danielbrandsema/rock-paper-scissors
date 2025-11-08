b43a4331613f0e55f5db9d21bfe38391b8960936
Daniël Brandsema <danielbrandsema@gmail.com>
2025-11-08 15:12:57 +0100

    Add getRandomInt function to create outputs
    the Math function gives 1, 2 or 3 randomly as output.
    after connecting these outputs to the three variables "rock", "paper" and "scissors",
    the getComputerChoice function also randomly outputs these variables.
    question mark: the 1/2/3-ouputs do not always correspond with the variables they are connected to...

--- javasctipt.js
+++ javasctipt.js
@@ -1,9 +1,19 @@
+function getRandomInt(min, max) {
+  const minCeiled = Math.ceil(1);
+  const maxFloored = Math.floor(4);
+  return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // The maximum is exclusive and the minimum is inclusive
+}
+
 const a = "rock";
 const b = "paper";
 const c = "scissors";
 
 function getComputerChoice(a,b,c) {
-    return a || b || c;
+    if (getRandomInt() == 1) {
+        return a;
+    } else if (getRandomInt() == 2) {
+        return b;
+    } else return c;
  }
 
  console.log(getComputerChoice(a,b,c));  
